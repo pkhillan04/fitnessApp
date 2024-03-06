03100ccf0a2483562ef3ad33c8dc32d94e7497b4
Part <pkhillan1@gmail.com>
2024-03-05 21:00:15 -0600

    Gave project a fancy name

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -9,9 +9,9 @@ const HeroBanner = () => {
         <Box sx={{
             mt: { lg: '212px', xs: '70px' },
             ml: { sm: '50px' }
-        }} position="relative" p="20px">
-            <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-                Fitness Club
+        }} position="relative" p="0px">
+            <Typography color="#FF2625" fontWeight="600" fontSize="40px">
+                Mighty-Moves
             </Typography>
             <Typography fontWeight={700}
                 sx={{
